0eaffe7600283dafa93defe564e2efef39ce6500
Mensky <bv_1514@mail.ru>
2015-11-29 20:45:45 +0300

    Added view photos

--- OnlineStore/src/main/webapp/jsp/js/animation.js
+++ OnlineStore/src/main/webapp/jsp/js/animation.js
@@ -1,5 +1,23 @@
+/* Animation for drop menu*/
 $(document).ready(function() {
 	$('#123').hover(function() {
 		$(this).find('ul>li').stop().fadeToggle(300);
 	});
+});
+
+/*View product photo*/
+$(document).ready(function(){
+
+
+
+	$(".thumbs a").click(function(){
+	
+		var largePath = $(this).attr("href");
+		var largeAlt = $(this).attr("title");
+		
+		$("#largeImg").attr({ src: largePath, alt: largeAlt });
+		
+		return false;
+	});
+	
 });
