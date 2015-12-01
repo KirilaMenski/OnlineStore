5184286861e9d33279cb3f890852ede81c1b375b
Mensky <bv_1514@mail.ru>
2015-12-01 22:23:07 +0300

    Added controller for pages main,products and view_product

---
+++ OnlineStore/src/main/java/by/ansgar/store/controllers/MainPageController.java
@@ -0,0 +1,33 @@
+package by.ansgar.store.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("serial")
+@WebServlet("/main")
+public class MainPageController extends HttpServlet {
+
+	private static final Logger LOG = Logger.getLogger(MainPageController.class);
+	
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException{
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/main.jsp");
+		dispatcher.forward(request, response);
+		LOG.info("Redirect to main.jsp");
+	}
+
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException{
+
+	}
+
+}
