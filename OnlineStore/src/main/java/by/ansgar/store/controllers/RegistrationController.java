3a8afa46ecf9f336598eafb1c1027b748107e14a
Mensky <bv_1514@mail.ru>
2015-12-03 22:18:25 +0300

    Controller for registration

---
+++ OnlineStore/src/main/java/by/ansgar/store/controllers/RegistrationController.java
@@ -0,0 +1,62 @@
+package by.ansgar.store.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import by.ansgar.store.entity.User;
+import by.ansgar.store.service.UserService;
+import by.ansgar.store.service.impl.UserServiceImpl;
+
+@SuppressWarnings("serial")
+@WebServlet("/registration")
+public class RegistrationController extends HttpServlet {
+
+	private static final Logger LOG = Logger
+			.getLogger(RegistrationController.class);
+
+	private UserService userService = new UserServiceImpl();
+	private User user = new User();
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		try {
+			String nickName = request.getParameter("nick_name");
+			String email = request.getParameter("email");
+			String password = request.getParameter("rep_password");
+			String role = "user";
+
+			user.setNickName(nickName);
+			user.setEmail(email);
+			user.setPassword(password);
+			user.setRole(role);
+
+			userService.addUser(user);
+			LOG.info("User " + nickName + " registrate succsesffuly");
+
+			request.setAttribute("nick_name", nickName);
+			RequestDispatcher dispathcer = request
+					.getRequestDispatcher("jsp/main.jsp");
+			dispathcer.forward(request, response);
+			
+
+		} catch (SQLException e) {
+			LOG.error("Registration error!", e);
+		}
+
+	}
+
+}
