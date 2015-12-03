71e2ed01e7b75db72fa1762b67d08f5d7b453aca
Mensky <bv_1514@mail.ru>
2015-12-03 22:16:36 +0300

    Added script for animation log in and registration

--- OnlineStore/src/main/webapp/jsp/js/animation.js
+++ OnlineStore/src/main/webapp/jsp/js/animation.js
@@ -5,19 +5,37 @@ $(document).ready(function() {
 	});
 });
 
-/*View product photo*/
-$(document).ready(function(){
+/* View product photo */
+$(document).ready(function() {
 
+	$(".thumbs a").click(function() {
 
-
-	$(".thumbs a").click(function(){
-	
 		var largePath = $(this).attr("href");
 		var largeAlt = $(this).attr("title");
-		
-		$("#largeImg").attr({ src: largePath, alt: largeAlt });
-		
+
+		$("#largeImg").attr({
+			src : largePath,
+			alt : largeAlt
+		});
+
 		return false;
 	});
-	
+
+});
+
+/* Show enter div when clicked href log_in */
+$(document).ready(function() {
+	$('#log_in, #close_reg').click(function(event) {
+		event.preventDefault(); // Для того чтобы при нажатии на ссылку не
+		// кидало вверх
+		$('#enter').slideToggle();
+	});
+});
+
+/* Show registrated div when clicked href registrate */
+$(document).ready(function() {
+	$('#registrate, #close_reg').click(function(event) {
+		event.preventDefault();
+		$('#registration_block').slideToggle();
+	});
 });
