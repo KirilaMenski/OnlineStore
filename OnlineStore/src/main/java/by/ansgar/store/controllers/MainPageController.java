887d68e05417c2271de3987d3784967e4db55a01
Mensky <bv_1514@mail.ru>
2015-12-04 21:47:33 +0300

    Added session

--- OnlineStore/src/main/java/by/ansgar/store/controllers/MainPageController.java
+++ OnlineStore/src/main/java/by/ansgar/store/controllers/MainPageController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
@@ -20,6 +21,10 @@ public class MainPageController extends HttpServlet {
 	public void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException{
 		
+		HttpSession session = request.getSession();
+		String sessionValue = (String)session.getAttribute("nickName");
+
+		request.setAttribute("nick_name", sessionValue);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/main.jsp");
 		dispatcher.forward(request, response);
 		LOG.info("Redirect to main.jsp");
