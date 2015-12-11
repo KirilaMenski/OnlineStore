89c42edfc97d68b984d36ba7590f329ee48a8419
Mensky <bv_1514@mail.ru>
2015-12-11 21:39:57 +0300

    Added new command

--- OnlineStore/src/main/java/by/ansgar/store/command/CommandEnum.java
+++ OnlineStore/src/main/java/by/ansgar/store/command/CommandEnum.java
@@ -1,33 +1,49 @@
 package by.ansgar.store.command;
 
+import by.ansgar.store.command.impl.LogInCommand;
+import by.ansgar.store.command.impl.LogOutCommand;
 import by.ansgar.store.command.impl.MainPageCommand;
 import by.ansgar.store.command.impl.ProductsPageCommand;
 import by.ansgar.store.command.impl.ProfilePageCommand;
+import by.ansgar.store.command.impl.RegistrationCommand;
 import by.ansgar.store.command.impl.ViewProductPageCommand;
 
 public enum CommandEnum {
 
 	MAIN {
 		{
-			 this.command = new MainPageCommand();
+			this.command = new MainPageCommand();
 		}
-
 	},
 
 	PRODUCTS {
 		{
-			 this.command = new ProductsPageCommand();
+			this.command = new ProductsPageCommand();
 		}
-
 	},
-	VIEW_PRODUCT{
+	VIEW_PRODUCT {
 		{
 			this.command = new ViewProductPageCommand();
 		}
 	},
-	PROFILE{
+	PROFILE {
 		{
 			this.command = new ProfilePageCommand();
+		}
+	},
+	LOGIN {
+		{
+			this.command = new LogInCommand();
+		}
+	},
+	LOGOUT {
+		{
+			this.command = new LogOutCommand();
+		}
+	},
+	REGISTRATION {
+		{
+			this.command = new RegistrationCommand();
 		}
 	};
 
