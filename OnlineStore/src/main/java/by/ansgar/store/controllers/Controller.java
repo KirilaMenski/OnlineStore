4a0d67817d0ad164bd9920b042a1304555f94ad2
Mensky <bv_1514@mail.ru>
2015-12-11 21:40:39 +0300

    Added session

--- OnlineStore/src/main/java/by/ansgar/store/controllers/Controller.java
+++ OnlineStore/src/main/java/by/ansgar/store/controllers/Controller.java
@@ -1,18 +1,16 @@
 package by.ansgar.store.controllers;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 import by.ansgar.store.command.Command;
 import by.ansgar.store.command.CommandFactory;
@@ -40,32 +38,37 @@ public class Controller extends HttpServlet {
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-//		ServletContext context = config.getServletContext();
-//		String log4jConfigFile = context
-//				.getInitParameter("log4j-config-location");
-//		String fullPath = context.getRealPath("") + File.separator
-//				+ log4jConfigFile;
-//
-//		PropertyConfigurator.configure(fullPath);
-//		
-//		System.out.println(fullPath);
+		// ServletContext context = config.getServletContext();
+		// String log4jConfigFile = context
+		// .getInitParameter("log4j-config-location");
+		// String fullPath = context.getRealPath("") + File.separator
+		// + log4jConfigFile;
+		//
+		// PropertyConfigurator.configure(fullPath);
+		//
+		// System.out.println(fullPath);
 
 	}
 
 	public void processComand(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
-//		LOG.info("The project sturtup");
-		
 		Command command = CommandFactory.defineCommand(request);
 		String page = command.execute(request);
 		response.setHeader("Cache-Control",
 				"no-cache, no-store, must-revalidative");
 		response.setHeader("Pragma", "no-cache");
 		response.setHeader("Expires", "0");
+		HttpSession session = request.getSession();
+		String sessionValue = (String) session.getAttribute("nick_name");
+		if(sessionValue != null){
+			request.setAttribute("nick_name", sessionValue);
+		}else{
+			request.setAttribute("nick_name", "Guest");
+		}
 		if (page != null) {
 			request.getRequestDispatcher(page).forward(request, response);
-		}else{
-			
+		} else {
+
 		}
 
 	}
