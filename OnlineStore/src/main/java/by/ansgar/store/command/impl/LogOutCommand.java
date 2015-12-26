aaba970374e7f962ae97862a4fb48588b5ef5adb
Mensky <bv_1514@mail.ru>
2015-12-26 20:34:35 +0300

    Added logout from session

--- OnlineStore/src/main/java/by/ansgar/store/command/impl/LogOutCommand.java
+++ OnlineStore/src/main/java/by/ansgar/store/command/impl/LogOutCommand.java
@@ -1,13 +1,22 @@
 package by.ansgar.store.command.impl;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
 
 import by.ansgar.store.command.Command;
 
 public class LogOutCommand implements Command{
 
+	private static final Logger LOG = Logger.getLogger(LogOutCommand.class);
+	
 	public String execute(HttpServletRequest request) {
-		return null;
+		String page = "controller?command=main";
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		return page;
 	}
 
 }
